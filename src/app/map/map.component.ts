3b0fe0b4c8025a14eaf1cd8c3439b3551ad03e2e
Serch <sergioibanezmoreno@gmail.com>
2018-04-08 18:31:55 +0100

    masssss estilos y le hemos anadido la captura de fotos para movil, a ver como funciona

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,7 +13,7 @@ import { UserService } from '../user.service';
 })
 
 export class MapComponent implements OnInit {  
-  @ViewChild('gmap') gmapElement: any;
+  // No creo que necesitemos mapa aqui @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
 
   authorised: boolean;
@@ -44,12 +44,14 @@ export class MapComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    /* No creo que necesitemos mapa aqui
     var mapProp = {
       center: new google.maps.LatLng(18.5793, 73.8143),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    */
     this.checkAuthorization()
   }
 
