1d9166c81d5ed00f4e6682f229aeff337c27d61d
Serch <sergioibanezmoreno@gmail.com>
2018-04-17 20:47:27 +0100

    anadido un alert para probar en movil el spinner al cambiar foto. no funciona

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -107,6 +107,7 @@ export class MapComponent implements OnInit {
   setPhotoFile(fileData) {
     if (fileData.target.files && fileData.target.files[0]) {
       this.spinnerService.show();
+      console.log('1')
       this.userPhotoFile = fileData.target.files[0];
       var nameFile = this.userPhotoFile.name;
       var typeFile = this.userPhotoFile.type;
@@ -125,6 +126,7 @@ export class MapComponent implements OnInit {
               })
                 .then((photoFile) => {
                   this.userPhotoFile = photoFile;
+                  alert('2')
                   this.spinnerService.hide();
                 })
             }) 
